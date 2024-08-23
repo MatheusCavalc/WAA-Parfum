05fb7581e8f366fdf392934276cdef090a4c08dd
Matheus Cavalcante <matheuscavalcs@gmail.com>
2024-08-23 10:04:36 -0300

    navbar and footer

---
+++ layouts/default.vue
@@ -0,0 +1,162 @@
+<script setup>
+import { ref, onMounted, onBeforeUnmount } from 'vue';
+
+const menu = ref(false)
+
+const isHidden = ref(false);
+let lastScrollTop = 0;
+
+const isNavbarOpaque = ref(false);
+
+const handleScroll = () => {
+    const scrollThreshold = 100;
+    isNavbarOpaque.value = window.scrollY > scrollThreshold;
+
+    const currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (currentScrollTop > lastScrollTop) {
+        // Scroll para baixo
+        isHidden.value = true;
+    } else {
+        // Scroll para cima
+        isHidden.value = false;
+    }
+
+    lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop;
+};
+
+onMounted(() => {
+    window.addEventListener('scroll', handleScroll);
+});
+
+onBeforeUnmount(() => {
+    window.removeEventListener('scroll', handleScroll);
+});
+</script>
+
+<template>
+    <nav :class="{ 'border-b border-b-black bg-black': isNavbarOpaque, '-translate-y-full ease-in': isHidden, '-translate-y-0 ease-out': !isHidden }"
+        class="fixed top-0 z-30 w-full transition duration-700 ease-in-out">
+        <div class="flex flex-wrap items-center justify-between max-w-screen-2xl px-1 lg:px-4 py-3 lg:py-0 mx-auto">
+            <NuxtLink to="/" class="flex items-center gap-1 transition duration-700 ease-in-out text-white">
+                <img src="../assets/logo-nobg.png" class="h-8 w-12 object-cover">
+                <span
+                    class="self-center text-xl font-semibold lg:text-2xl whitespace-nowrap traced-black text-secondary">WAA
+                    Parfum</span>
+            </NuxtLink>
+
+            <div class="lg:order-3">
+                <a href="https://wa.me/+5585997249761" target="_blank">
+                    <button type="button"
+                        class="px-4 py-2 mr-1 text-sm font-medium text-center text-white bg-black border border-black rounded-lg lg:px-4 hover:bg-transparent hover:text-gray-400 hover:border hover:border-black transition ease-in-out duration-300 lg:mr-0">
+                    Compre Agora</button>
+                </a>
+            </div>
+
+            <div class="flex lg:order-2">
+                <button @click="menu = !menu" :class="{ 'opened': menu }"
+                    class="inline-flex items-center stroke-white justify-center w-10 h-10 p-1 text-sm rounded-lg lg:hidden transition duration-700 ease-in-out">
+                    <svg width="100" height="100" viewBox="0 0 100 100">
+                        <path class="line line1"
+                            d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058" />
+                        <path class="line line2" d="M 20,50 H 80" />
+                        <path class="line line3"
+                            d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942" />
+                    </svg>
+                </button>
+            </div>
+
+            <div :class="{ 'lg:block': menu, 'hidden lg:block': !menu }" class="w-full lg:w-auto lg:order-1"
+                id="navbar-default">
+                <ul :class="{ 'bg-black': isNavbarOpaque, '': !isNavbarOpaque }"
+                    class="flex flex-col transition duration-700 ease-in-out p-4 mt-4 font-medium border border-gray-100 rounded-lg lg:p-0 lg:flex-row lg:space-x-14 lg:mt-0 lg:border-0 lg:bg-transparent">
+                    <NuxtLink to="/">
+                        <li>
+                            <button class="block py-2 pl-3 pr-4 rounded lg:border-0 lg:py-5 text-white">
+                                Sobre
+                            </button>
+                        </li>
+                    </NuxtLink>
+                    <NuxtLink to="/">
+                        <li>
+                            <button class="block py-2 pl-3 pr-4 rounded lg:border-0 lg:py-5 text-white">
+                                Produtos
+                            </button>
+                        </li>
+                    </NuxtLink>
+                    <NuxtLink to="/">
+                        <li>
+                            <button class="block py-2 pl-3 pr-4 rounded lg:border-0 lg:py-5 text-white">
+                                Contato
+                            </button>
+                        </li>
+                    </NuxtLink>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div>
+        <slot />
+    </div>
+</template>
+
+<style>
+.menu {
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+    display: flex;
+    padding: 0;
+}
+
+.line {
+    fill: none;
+    stroke-width: 7;
+    transition: stroke-dasharray 500ms cubic-bezier(0.4, 0, 0.2, 1),
+        stroke-dashoffset 500ms cubic-bezier(0.4, 0, 0.2, 1);
+}
+
+.line1 {
+    stroke-dasharray: 60 207;
+    stroke-width: 7;
+}
+
+.line2 {
+    stroke-dasharray: 60 60;
+    stroke-width: 7;
+}
+
+.line3 {
+    stroke-dasharray: 60 207;
+    stroke-width: 7;
+}
+
+.opened .line1 {
+    stroke-dasharray: 90 207;
+    stroke-dashoffset: -134;
+    stroke-width: 6;
+}
+
+.opened .line2 {
+    stroke-dasharray: 1 60;
+    stroke-dashoffset: -30;
+    stroke-width: 6;
+}
+
+.opened .line3 {
+    stroke-dasharray: 90 207;
+    stroke-dashoffset: -134;
+    stroke-width: 6;
+}
+
+.v-enter-active,
+.v-leave-active {
+    transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+    opacity: 0;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,52 @@
 <template>
-    <p class="underline">Oi</p>
+    <NuxtLayout>
+
+
+        <div class="min-h-screen bg-black text-white bg-home bg-center">
+            <div class="bg-gradient-to-b from-[#0000008f] to-[#0000008f] min-h-screen pt-16"></div>
+        </div>
+
+        <div class="bg-black h-screen"></div>
+
+        <div>
+            <section class="h-1/3 md:h-4/5 bg-footer py-20">
+                <div class="py-40 mx-4 md:mx-20">
+                    <div class="flex justify-center">
+                        <p class="text-2xl md:text-2xl lg:text-4xl font-bold text-center text-white">
+                            Conheça nossas redes sociais
+                        </p>
+                    </div>
+
+                    <div class="flex justify-center pb-20 md:pb-44 mt-3">
+                        <div class="absolute">
+                            <a href="https://www.instagram.com/waaperfumes_/" target="_blank">
+                                <IconsInstagramIcon class="size-14 fill-white hover:scale-110 transition duration-150" />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+
+
+        <!--Test
+        <div class="min-h-screen bg-green-600">
+            <img src="../assets/logo-nobg.png" class="" alt="">
+        </div>
+        -->
+    </NuxtLayout>
 </template>
+
+<style scoped>
+.bg-home {
+    background-image: url('../assets/home.jpeg');
+}
+
+.bg-footer {
+    background-size: cover;
+    background-position: center;
+    width: 100%;
+    background-image: linear-gradient(#8a8a8a80, #9333ea), url('../assets/footer.jpeg');
+    background-repeat: no-repeat;
+}
+</style>
